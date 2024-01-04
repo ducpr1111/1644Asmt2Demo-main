81d8e41b49100f796949ebdc3bb0b83b7b0252ad
ducpr1111 <133930254+ducpr1111@users.noreply.github.com>
2024-01-04 23:57:19 +0700

    initial commit

---
+++ models/ProductModel.js
@@ -0,0 +1,11 @@
+var mongoose = require ('mongoose');
+var ProductSchema = mongoose.Schema({
+   name: String,
+   price: Number,
+   category: String,
+   class: String,
+   image: String,
+   stock: Number
+});
+const ProductModel = mongoose.model('product', ProductSchema, 'product');
+module.exports = ProductModel;
